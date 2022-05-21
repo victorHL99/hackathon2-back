bf35de64a1073968d3f18f4952e0a7ee413ae6d8
victorHL99 <victor.lessa@estudante.ufla.br>
2022-05-20 21:46:37 -0300

    arrumando pc

--- src/controllers/inicioController.js
+++ src/controllers/inicioController.js
@@ -1,3 +1,12 @@
+import connectMongoDB from "../../mongoConection.js";
+
 export async function incio(req, res){
-    res.send("OK");
+    const { banco_dados } = await connectMongoDB();
+    try{
+        const resultados = await banco_dados.collection("assuntos").find({}).toArray();
+        res.send(resultados);
+    }
+    catch(erro){
+        res.send(erro);
+    }
 }
